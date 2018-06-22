288447dba32c85fe6a5fe1a4c903c391bc6d3276
Landon Abney <Landonabney@gmail.com>
2018-06-21 22:54:03 -0700

    Calculate gains/losses on the fly
    It's doubtful that this will be a performance issue for our server, and 
    as I plan to replace this joke of a DB with something better eventually 
    this will make it simpler to fix later.

--- dbActions.js
+++ dbActions.js
@@ -6,7 +6,7 @@ const db = low(adapter);
 db.defaults({ users: [] }).write();
 
 const createUser = userId => db.get('users').push({
-  id: userId, gains: 0, losses: 0, history: [],
+  id: userId, history: [],
 }).write();
 
 const getUser = (userId) => {
@@ -18,24 +18,30 @@ const getUser = (userId) => {
   return value;
 };
 
-const updateWinnings = (userId, change) => {
-  const user = db.get('users').find({ id: userId });
-  const userValues = user.value();
-  if (change < 0) user.assign({ losses: userValues.losses + Math.abs(change) }).write();
-  if (change > 0) user.assign({ gains: userValues.gains + change }).write();
-};
-
 const addHistory = (userId, change, total, timestamp) => {
   if (!userId || !change || !total || !timestamp) return;
   getUser(userId);
   db.get('users').find({ id: userId }).get('history').push({ timestamp, change, total })
     .write();
-  updateWinnings(userId, change);
 };
 
 const getHistory = userId => getUser(userId).history;
-const getLosses = userId => getUser(userId).losses;
-const getGains = userId => getUser(userId).gains;
+
+const getLosses = userId =>
+  getHistory(userId).reduce((total, i) => {
+    if (i.change < 0) {
+      return total + Math.abs(i.change);
+    }
+    return total;
+  }, 0);
+
+const getGains = userId =>
+  getHistory(userId).reduce((total, i) => {
+    if (i.change > 0) {
+      return total + i.change;
+    }
+    return total;
+  }, 0);
 
 exports.getUser = getUser;
 exports.addHistory = addHistory;
